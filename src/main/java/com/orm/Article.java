e09dfdbe4af7927c4943e241cc740bf15afaacd2
tangtaiming <accountnum@sina.cn>
2016-03-04 18:14:24 +0800

    文章实体

---
+++ src/main/java/com/orm/Article.java
@@ -0,0 +1,79 @@
+package com.orm;
+/**
+ * ÎÄÕÂ
+ * @author ÌÆÌ«Ã÷
+ *
+ */
+public class Article {
+
+	private Integer id;
+	
+	private String title;
+	
+	private String titleDate;
+	
+	private String inputDate;
+	
+	private String url;
+	
+	private String writer;
+	
+	private String writerId;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitleDate() {
+		return titleDate;
+	}
+
+	public void setTitleDate(String titleDate) {
+		this.titleDate = titleDate;
+	}
+
+	public String getInputDate() {
+		return inputDate;
+	}
+
+	public void setInputDate(String inputDate) {
+		this.inputDate = inputDate;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getWriterId() {
+		return writerId;
+	}
+
+	public void setWriterId(String writerId) {
+		this.writerId = writerId;
+	}
+	
+}

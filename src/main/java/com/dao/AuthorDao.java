db139a2d322ea16e45e27afa20eb836100e9ef5b
shejun <shejun1123@163.com>
2016-03-10 22:53:31 +0800

    111

--- src/main/java/com/dao/AuthorDao.java
+++ src/main/java/com/dao/AuthorDao.java
@@ -1,4 +1,4 @@
-package com.dao;
+﻿package com.dao;
 
 import com.orm.Author;
 
@@ -6,5 +6,6 @@ import com.orm.Author;
  * Created by 佘俊 on 2016/3/10.
  */
 public interface AuthorDao {
-    public Author fetchAuthorById();
+    public Author fetchAuthorById(); 
+
 }
